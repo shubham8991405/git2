02a9b669ab8608928560602ecc342a93023d7591
shubham8991405 <shubham8991405@gmail.com>
2023-02-02 09:40:50 +0530

    added

---
+++ src/animal/animal.java
@@ -0,0 +1,29 @@
+package animal;
+
+abstract class animal {animal(){
+	System.out.println("all animal");}
+{
+	public abstract void sound();
+}
+class Dog extends animal
+{
+	public void sound() {
+		System.out.println("dog is bark");
+	}
+	class Lion extends animal 
+	{
+		public void sound()
+		class Lion extends animal
+	{
+			public void sound() {
+				System.out.println("lion  is bark");
+}
+class Test
+{public static void main(String[] args) {
+	
+	Dog d =new Dog();
+	Lion l=new Lion();
+	d.sound(); l.sound();
+}
+}
+}
